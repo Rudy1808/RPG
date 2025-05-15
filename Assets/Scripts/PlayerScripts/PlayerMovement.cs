d59486b471fde82403bc47196ea486bb06dd7d2a
Czarnuszek09 <jakub.t.kaminski.09@gmail.com>
2025-05-15 22:16:59 +0200

    Ruch gracza
    Ruch gracza po kratkach

---
+++ Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    public float walkSpeed = 5f;
+    public float sprintSpeed = 7f;
+    private bool isMoving = false;
+    private Vector2 input;
+
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        if (!isMoving)
+        {
+            input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+            if (input.x != 0) input.y = 0;
+
+            if (input != Vector2.zero)
+            {
+                StartCoroutine(Move());
+            }
+        }
+        IEnumerator Move()
+        {
+            isMoving = true;
+
+            float currentSpeed = Input.GetKey(KeyCode.LeftShift) ? sprintSpeed : walkSpeed;
+            float duration = 1f / currentSpeed;
+
+            Vector3 startPosition = transform.position;
+            Vector3 endPosition = startPosition + new Vector3(input.x, input.y, 0);
+
+            float elapsedTime = 0f;
+
+            while (elapsedTime < duration)
+            {
+                transform.position = Vector3.Lerp(startPosition, endPosition, elapsedTime / duration);
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+
+            transform.position = endPosition;
+            isMoving = false;
+        }
+    }
+}
