2de0d29ecc8176615fd35dc8573b8bfcc0c6249b
Blazo810 <mocny.blazer@gmail.com>
2025-06-18 22:25:35 +0200

    invetoryUI v chuj

--- Assets/Scripts/UI/InventoryUI/InventoryUIManager.cs
+++ Assets/Scripts/UI/InventoryUI/InventoryUIManager.cs
@@ -6,62 +6,27 @@ using UnityEngine.UIElements;
 
 public class InventoryUIManager : MonoBehaviour
 {
+    public UIDocument UI;
 
-    //elementy
-    public VisualElement root;
-    public VisualElement inventoryGrid;
-
-    //scalowanie
-    public int baseWidth = 640;
-    public int baseHeigh = 360;
-
-    public int scaleX;
-    public int scaleY;
-    public int scaleFactor;
-
-    private List<VisualElement> slotList = new List<VisualElement>();
-    private int slotNumber = 28;
     private void Start()
     {
-        InitializeUI();
-        BuildInventoryGrid();
-        ScalingUI();
-
+        Scaling();
     }
-    private void InitializeUI()
+    private void Scaling()
     {
-        root = GetComponent<UIDocument>()?.rootVisualElement;
-        inventoryGrid = root.Q<VisualElement>("InventoryGrid");
+        int width = 640;
+        int height = 360;
 
-        if (inventoryGrid == null)
+        int scalingValue = Mathf.Min(Screen.width / width, Screen.height / height);
+        
+        if (scalingValue < 1)
         {
-            Debug.LogError("Brak VisualElement o nazwie 'InventoryGrid'");
+            return;
         }
-    }
-    private void ScalingUI()
-    {
-        scaleX = Screen.width / baseWidth;
-        scaleY = Screen.height / baseHeigh;
+        var root = UI.rootVisualElement;
 
-        scaleFactor = math.min(scaleX, scaleY);
-
-        root.style.width = baseWidth;
-        root.style.height = baseHeigh;
-        root.style.scale = new Scale(new Vector2(scaleFactor,scaleFactor));
-
-    }
-    private void BuildInventoryGrid()
-    {
-        for (int i = 0; i < slotNumber; i++)
-        {
-            var slot = new VisualElement();
-            slot.AddToClassList("slot");
-            inventoryGrid.Add(slot);
-            slotList.Add(slot);
-        }
-    }
-    private void Refresh()
-    {
+        root.style.width = width * scalingValue;
+        root.style.height = height * scalingValue;
 
     }
 }
