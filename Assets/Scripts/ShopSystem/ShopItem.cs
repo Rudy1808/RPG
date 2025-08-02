3c015a17031dec9628ee4d73bca30c7e14bbab51
Rudy1808 <chabasinskimarcel796@gmail.com>
2025-08-02 15:30:48 +0200

    a

--- Assets/Scripts/ShopSystem/ShopItem.cs
+++ Assets/Scripts/ShopSystem/ShopItem.cs
@@ -2,5 +2,8 @@ using UnityEngine;
 
 public class ShopItem : Item
 {
-    
+    public int Id;
+    //ItemDatabase.GetItemByID(ID);
+    public int Price {  get; private set; }
+    public float PriceModifier = 1f;
 }
